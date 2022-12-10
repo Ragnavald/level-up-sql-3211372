e6393268d791f67b1f5b1c9b42d532b1e3594c40
Ronaldo Ávila de Arruda Junior <110701606+Ragnavald@users.noreply.github.com>
2022-12-10 03:30:38 +0000

    Desafios

--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -2,3 +2,6 @@
 
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
+INSERT INTO CustomerParty  VALUES((SELECT CustomerID FROM Customers WHERE Email = 'atapley2j@kinetecoinc.com'),4);
+SELECT * FROM Customers JOIN CustomerParty ON Customers.CustomerID = CustomerParty.CustomerID
+WHERE Email = 'atapley2j@kinetecoinc.com' 
--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -5,3 +5,17 @@
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+
+
+SELECT * FROM Reservations JOIN Customers ON
+Reservations.CustomerID = Customers.CustomerID WHERE
+Customers.Lastname Like 'St%' and Reservations.PartySize  = '4'
+and Date >='2022-06-14'
+
+-- O código seria melhor se a data abrangesse reservas anteriores também, uma vez que
+-- o cliente pode ter perdido o prazo
+
+SELECT * FROM Reservations JOIN Customers ON
+Reservations.CustomerID = Customers.CustomerID WHERE
+Customers.Lastname Like 'St%' and Reservations.PartySize  = '4'
+ORDER BY Date DESC
